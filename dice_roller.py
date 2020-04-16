4207e6c8b5ceb56fa1dcd61e8c0f1d3a6bb1e53a
RandomUser42C-137 <63723712+RandomUser42C-137@users.noreply.github.com>
2020-04-16 17:57:09 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
+import sys
+
+sys.path.append('C:\\Program Files\\Git\\emoji')
+print(sys.path)
+
+import random
+import emoji
+
 def main():
-  print('You rolled a die')
+    roll = random.randint(1, 6)
+    print(f'You rolled a {roll}')
+    print(emoji.emojize(':thumbs_up:'))
 
 if __name__== "__main__":
   main()
