bbc1cb959b08d0f22acebe6be653321508b2c474
RandomUser42C-137 <63723712+RandomUser42C-137@users.noreply.github.com>
2020-04-16 18:35:35 +0530

    Added rolls and size

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,19 @@
 import random
 
 def main():
-    dice_rolls = 2
+    dice_rolls = int(input('How many rolls bro?'))
+    dice_size = int(input('How many sides broo?'))
     dice_sum = 0
     for i in range(0, dice_rolls):
-        roll = random.randint(1, 6)
-        print(f'Whoaaa you just rolled a {roll}')
+        roll = random.randint(1, dice_size)
         dice_sum += roll
+        if roll == 1:
+         print(f'You rolled a {roll}! Critical Failure!!')
+        elif roll == 6:
+         print(f'You rolled a {roll}! Critical Sucess!')
+        else:
+         print(f'Whoaaa you just rolled a {roll}!')
+
     print(f'You have a total of {dice_sum}')
 
 if __name__== "__main__":
