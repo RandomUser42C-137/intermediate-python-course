0b34d48d172013072fcc06ea22b3787e4c7a2e1b
RandomUser42C-137 <63723712+RandomUser42C-137@users.noreply.github.com>
2020-04-16 18:25:53 +0530

    Added sums and rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,13 @@
-import sys
-
-sys.path.append('C:\\Program Files\\Git\\emoji')
-print(sys.path)
-
 import random
-import emoji
 
 def main():
-    roll = random.randint(1, 6)
-    print(f'You rolled a {roll}')
-    print(emoji.emojize(':thumbs_up:'))
+    dice_rolls = 2
+    dice_sum = 0
+    for i in range(0, dice_rolls):
+        roll = random.randint(1, 6)
+        print(f'Whoaaa you just rolled a {roll}')
+        dice_sum += roll
+    print(f'You have a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
